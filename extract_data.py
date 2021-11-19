6ac17acd56f2fba594b9036adb3e48ec380b8548
Anshuman-Raj <anshuman.raj10@gmail.com>
2021-11-19 18:33:01 +0530

    Project completion

--- extract_data.py
+++ extract_data.py
@@ -7,12 +7,10 @@ bucket = "encrypted-medical-predictions"
 
 
 def download_data(bucket, key, filename):
-    downloader = boto3.client('s3', aws_access_key_id='AKIA4HBPW4JSZBT3YY4W',
-                              aws_secret_access_key='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')
+    downloader = boto3.client('s3' )
     downloader.download_file(bucket, key, filename)
 
 
 def upload_data(bucket, key, filename):
-    downloader = boto3.client('s3', aws_access_key_id='AKIA4HBPW4JSZBT3YY4W',
-                              aws_secret_access_key='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')
+    downloader = boto3.client('s3')
     downloader.upload_file(filename, bucket, key)
